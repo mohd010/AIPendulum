b8fe169f687e9ede9323e9ba276b5cbac196a6bf
Mohammad Al Khatib <mohammad.alkhatib@rptu.de>
2024-12-18 15:18:42 +0100

    structure the pendulum file using some comments

---
+++ pendulum.py
@@ -0,0 +1,10 @@
+# introduce the model of a pendulum
+
+# generate training and testing data from the model
+
+# mimic the model via a neural network
+
+# design a controller using a neural network to stabilize
+# the pendulum vertically
+
+
